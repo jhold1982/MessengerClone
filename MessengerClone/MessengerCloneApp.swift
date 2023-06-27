124cd4424b155fab9167068ada087efa7faf84f8
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2023-06-27 03:47:54 -0500

    Initial Commit

---
+++ MessengerClone/MessengerCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerCloneApp.swift
+//  MessengerClone
+//
+//  Created by Justin Hold on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct MessengerCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
